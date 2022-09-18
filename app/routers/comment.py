ea0458cd241f8f24d7572dd8eac39d5d3933b377
Makim1998 <mmijatovic32@gmail.com>
2022-09-18 18:52:09 +0200

    feat(comment): added controler for crud operations for comments

---
+++ app/routers/comment.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from app.dto import CommentDTO, CommentCreateDTO
+from typing import List
+from app.services.comment_service import get_comments, add_comment
+
+router = APIRouter()
+
+
+@router.get("/comment/{post_id}", response_model=List[CommentDTO], tags=["comment"])
+async def get_comments_for_post(post_id):
+    return get_comments(post_id)
+
+
+@router.post("/comment", response_model=CommentCreateDTO, tags=["comment"])
+async def add(comment: CommentCreateDTO):
+    return add_comment(comment)
