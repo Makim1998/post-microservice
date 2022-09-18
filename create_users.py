c335eaf7e9ea2f24bc6e710cbf171889b67d418e
Makim1998 <mmijatovic32@gmail.com>
2022-09-18 18:40:05 +0200

    feat(comments): restructuring project

--- create_users.py
+++
@@ -1,7 +0,0 @@
-from main import Session, engine
-from model import User
-
-local_session = Session(bind=engine)
-user = User(username="nenafata", email='ahahahah')
-local_session.add(user)
-local_session.commit()
